fca8d186d8187d9296a8aee87991e6efe46b8655
zhaqj12 <zhaqj12@naver.com>
2022-07-06 14:30:37 +0900

    사칙연산

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -3,30 +3,83 @@
 section .text
 global CMAIN
 CMAIN:
-     mov rbp, rsp; for correct debugging
-
-    PRINT_STRING msg
+    mov rbp, rsp; for correct debugging
     
+    ; 어떤 숫자로 데이터를 받아오겠다.
+    GET_DEC 1, al
+    GET_DEC 1, num
+    
+      ;PRINT_DEC 1, al
+      ;NEWLINE
+      ;PRINT_DEC 1, num  
+      
+      ;더하기 연산
+      ;add a,b ( a= a + b )
+      ; a는 register or meomry
+      ; b는 register or 메모리 or 상수
+      ; -단! a,b 모두 메모리 X
+      
+      ;빼기 연산
+      ;sub a,b ( a= a - b )
+      
+      add al, 1 ;레지스터 + 상수
+      PRINT_DEC 1, al
+      NEWLINE
+      
+    ; mov rax, num
+      
+      add al, [num] ; 레지스터 + 메모리
+      PRINT_DEC 1, al
+      NEWLINE
+     
+      mov bl, 3
+      add al,bl ; 레지스터 + 레지스터      PRINT_DEC 1, al
+      PRINT_DEC 1, al
+      NEWLINE
+     
+      add [num], byte 1 ; 메모리 + 상수
+      PRINT_DEC 1, [num]
+      NEWLINE
+      
+       add [num], al ; 메모리 + 레지스터
+      PRINT_DEC 1, [num]
+      NEWLINE
+        
+        
+    ; 곱하기 연산
+    ; mul reg
+    ; - mul bl => al * bl 
+    ; -- 연산결과를 ax에 저장
+    ; - mul bx => ax * bx
+    ; -- 연산 결과를 dx(상위16비트) ax(하위16비트)에 저장
+    
+     ; ex ) 5 * 8은 ?
+     mov ax, 0
+     mov al, 5
+     mov bl , 8
+     mul bl
+     PRINT_DEC 1,ax
+     NEWLINE 
+         
+    ; 나누기 연산
+    ; div reg
+    ; - div bl => ax / bl
+    ; -- 연산 결과는 al(몫) ah(나머지)
+    
+    ;ex) 100/3은?
+    mov ax, 100
+    mov bl, 3
+    div bl
+    PRINT_DEC 1,al
+    NEWLINE
+    mov al, ah
+    PRINT_DEC 1,al     
+         
     xor rax, rax
     ret
 
-section .data
+;section .data
 
-; Hello World , 0x0 (0x0 문자가 끝났다)
- msg db 0x48,0x65,0x6c,0x6c,0x6f,0x20,0x57,0x6f,0x72,0x6c,0x64,0x0
- 
- a db 0x11,0x11,0x11,0x11 
-;{0x78,0x56,0x34,0x12 - 리틀 엔디안
-; 왜 뒤집어서 정렬을 하나? 
-; 장단점이 교차한다. -리틀 엔디언:캐스팅에 유리하다.
-;0x12345678 중 78만 남기고 다 날리고싶다고 한다면... 
-;첫번째 주소에서만 추출하면 된다.
-; 빅엔디언 : 숫자비교에 유리하다.
-; ex 0x123456 과 0x45123411 이 있다면 첫번째 byte 자리수로만 따져서 비교가 가능함 
- b dd 0x12345678
  
 section .bss
-;초기화 되지 않은 데이터
-; [변수이름] [크기] [개수]
-; 크기 resb(1) resw(2) resd(4) resq(8)
-e resb 10
+    num resb 1
