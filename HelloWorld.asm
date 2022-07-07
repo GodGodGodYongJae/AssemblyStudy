b201a0259683888d6900ae693cc09702ec5858cd
zhaqj12 <zhaqj12@naver.com>
2022-07-07 11:59:01 +0900

    주소와 배열

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -3,78 +3,70 @@
 section .text
 global CMAIN
 CMAIN:
-    mov rbp, rsp; for correct debugging
+   mov rbp, rsp; for correct debugging
     
-    ; 반복문 (while for )
-    ; 특정 조건을 만족할 때 까지 반복해서 실행
+   ; 배열과 주소
+   
+   ; 배열 : 동일한 타입의 데이터 묶음
+   ; - 배열을 구성하는 각 값을 배열 요소 (element)
+   ; - 배열의 위치를 가르키는 숫자 인덱스 (index)
+   mov rax ,a 
+   
+   PRINT_HEX 1, [a]
+   NEWLINE
     
-    ; ex ) Hello World 10번을 출력 해야 한다면?  
-    ; comment : 안보고 Jump 와 cmp를 활용해서 반복분 만들어보기.
+    ; 다음 데이터로 그럼 어떻게 넘어가는가? 위에 껀 a 에 있는 0x01이라면 0x02를 꺼내기 위해선?
+    ; 내가 생각한 것
+    ;inc rax
+    ;PRINT_HEX 1,[rax]
+    ; 풀이
+    PRINT_HEX 1, [a+1]
+    NEWLINE
+    PRINT_HEX 1, [a+2]
+    NEWLINE
     
-    ; mov al,10
-    ; 카운터와 관련된 건 ecx에서 많이 사용 
-    mov ecx, 10
-    ;jmp L1 빼도됨
+    ; 연습문제 : a 배열의 모든 데이터 출력
+    ; loop 형태로 구현. loop는 위로 한 줄만 입력이 가능..
+    xor ecx, ecx
+   
+    L1:
+    PRINT_HEX 1, [a+ecx]
+    NEWLINE
+    inc ecx
+    cmp ecx,5
+    jne L1
     
-L1:
-PRINT_STRING msg
-NEWLINE
-;sub ecx , 1
-;서브를 해줘도 되는데 dec이 더 빨리동작
-dec ecx
-cmp ecx, 0
-JNE L1
-              
-  ;연습문제 ) 1에서 100까지의 합을 구하는 프로그램 1+2+3+4 ... +100 = ?     
-  ; al 과 bl 등으로 하면 문제가 발생함. 
-  ;al (1바이트)를 결과 연산에 사용하시는데,
- ;signed int8은 (-127~128)의 범위라서 그 범위를 초과하면
- ;엉뚱한 결과가 나올 수 있습니다.         
-    xor eax,eax
-    mov ebx, 0
-L2:
-    inc eax
-    add ebx, eax
-    cmp eax, 100
+    
+    
+    ; 연습문제 :b 배열의 모든 데이터 출력
+    xor ecx,ecx
+
+    L2:
+    PRINT_HEX 2, [b+ecx*2]
+    NEWLINE
+    inc ecx
+    cmp ecx,5
     jne L2
     
-    PRINT_DEC 4, ebx
-    NEWLINE
-
-
-;PRINT_DEC 1, rbx
-;NEWLINE
-;해답
-; mov eax, 100
-; xor ebx,ebx
- 
-;  LABEL_SUM:
-;   ;inc ecx ; add ecx, 1과 동일
- ;  add ebx,eax ; ebx = ebx + ecx
-  ; dec eax
-   ;cmp eax, 0
-   ;jne LABEL_SUM
-   ;PRINT_DEC 4, ebx
-   ;NEWLINE
-   
-    ; loop [라벨]
-    ; - ecx에 반복 횟수
-    ; - loop를 할때마다 ecx 1 감소. 0이면 빠져나감. 아니면, 라벨로 이동
-    mov ecx,100
-    xor ebx,ebx
-LABEL_LOOP_SUM:
-      add ebx,ecx
-      loop LABEL_LOOP_SUM
-      
-    PRINT_DEC 4, ebx
-    NEWLINE
+;     PRINT_HEX 2, [b]
+ ;    PRINT_HEX 2, [b+2]
+  ;   PRINT_HEX 2, [b+4]
+   ;  PRINT_HEX 2, [b+6]
+    ; PRINT_HEX 2, [b+8]
+     
+     ; 주소
+     ;[시작주소 + 인덱스 + 크기]
     
     xor rax, rax
     ret
 
 section .data
     msg db 'Hello World', 0x0
- 
- 
+   a db 0x01,0x02,0x03,0x04,0x05 ; 5 * 1 = 5 byte
+;0x1 ;0x0
+;인디언 - 0x0001 로 볼수 있다.
+   b times 5 dw 1 ; 5 * 2 = 10 byte
+   
 section .bss
     num resb 1
+    
